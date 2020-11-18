35b8728e21174167ef15db4a65318bc07694c835
Luiz Guilherme de Medeiros Ventura <ziul_ventura@hotmail.com>
2020-11-18 14:29:54 +0100

    sfzCreatorClass.py - using class instead of dictionary for sample -> higher versatility.

--- sfzCreatorClass.py
+++ sfzCreatorClass.py
@@ -5,7 +5,19 @@ Created on Tue Nov 17 23:33:36 2020
 
 @author: luiz
 """
+import copy
 
+class sample:
+    # opcodes:
+    Fname              = None
+    Pkeycenter         = None
+    Lokey              = None
+    Hikey              = None
+    Lovel              = None
+    Hivel              = None
+    Ampeg_release      = None
+    Amp_veltrack       = None
+    
 
 class sfz_creator:
     Samples = None # list of dictionaries with sample name, file name and properties
@@ -25,32 +37,32 @@ class sfz_creator:
             
     def writeSampleBlock(self, sample):
         self.SfzStrL.append('<region>')
-        self.SfzStrL.append('sample=' + sample['fname'])
-        self.SfzStrL.append('lokey=' + str(sample['lokey']))
-        self.SfzStrL.append('hikey=' + str(sample['hikey']))
-        self.SfzStrL.append('lovel=' + str(sample['lovel']))
-        self.SfzStrL.append('hivel=' + str(sample['hivel']))
-        self.SfzStrL.append('pitch_keycenter=' + str(sample['pitch_keycenter']))
-        if sample['ampeg_release'] != None:
-            self.SfzStrL.append('ampeg_release=' + str(sample['ampeg_release']))
-        if sample['amp_veltrack'] != None:
-            self.SfzStrL.append('amp_veltrack=' + str(sample['amp_veltrack']))
+        self.SfzStrL.append('sample=' + sample.Fname)
+        self.SfzStrL.append('lokey=' + str(sample.Lokey))
+        self.SfzStrL.append('hikey=' + str(sample.Hikey))
+        self.SfzStrL.append('lovel=' + str(sample.Lovel))
+        self.SfzStrL.append('hivel=' + str(sample.Hivel))
+        self.SfzStrL.append('pitch_keycenter=' + str(sample.Pkeycenter))
+        if sample.Ampeg_release != None:
+            self.SfzStrL.append('ampeg_release=' + str(sample.Ampeg_release))
+        if sample.Amp_veltrack != None:
+            self.SfzStrL.append('amp_veltrack=' + str(sample.Amp_veltrack))
             
         self.SfzStrL.append('') # empty line
         
-samp1 = dict()
-samp1['fname'] = '/media/luiz/Volume/Downloads/SoundFonts/Cello SFZ/Samples/Ens-Cello-Leg83_a1.wav'
-samp1['lokey'] = 2
-samp1['hikey'] = 3
-samp1['lovel'] = 0
-samp1['hivel'] = 20
-samp1['pitch_keycenter'] = 64
-samp1['ampeg_release'] = 0.6
-samp1['amp_veltrack'] = None
+# Test:
+samp1 = sample()
+samp1.Fname = '/media/luiz/Volume/Downloads/SoundFonts/Cello SFZ/Samples/Ens-Cello-Leg83_a1.wav'
+samp1.Lokey = 2
+samp1.Hikey = 3
+samp1.Lovel = 0
+samp1.Hivel = 20
+samp1.Pkeycenter = 64
+samp1.Ampeg_release = 0.6
 
-samp2 = samp1.copy()
-samp2['pitch_keycenter'] = 68
-samp2['hikey'] = 10
+samp2 = copy.deepcopy(samp1)
+samp2.Pkeycenter = 68
+samp2.Hikey = 10
 
 sfz1 = sfz_creator([samp1, samp2], '/media/luiz/Volume/Downloads/SoundFonts/sfzPythonTest.sfz')
 sfz1.genSfz()
