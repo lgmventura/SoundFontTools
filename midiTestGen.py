801f1f2ccbf229324f50e966b1a4ba7b5dba15b8
lgmventura <ziul_ventura@hotmail.com>
2022-05-24 00:17:42 +0200

    midiTestGen.py after generating midi file, it can split the wav file into multiple files with correct names.

--- midiTestGen.py
+++ midiTestGen.py
@@ -23,6 +23,8 @@ def duration_decay(i):
 dur_list = []
 
 degrees  = [n.midi for n in notes]#[60, 62, 64, 65, 67, 69, 71, 72]  # MIDI note number
+
+# inserting some extra notes manually:
 degrees.insert(0,23)
 degrees.insert(0,21)
 
@@ -33,6 +35,8 @@ pause    = 2    # In beats
 #duration = 1    # In beats
 tempo    = 60   # In BPM
 vel   = 75  # 0-127, as per the MIDI standard
+
+#%% Generating midi file
 
 MyMIDI = MIDIFile(1)  # One track, defaults to format 1 (tempo track is created
                       # automatically)
@@ -47,3 +51,33 @@ for i, pitch in enumerate(degrees):
 
 with open("major-scale.mid", "wb") as output_file:
     MyMIDI.writeFile(output_file)
+    
+#%% Generate audio file from midi manually! Then replace the links and run this
+from pydub import AudioSegment
+import numpy as np
+from os import path
+
+audioPath = r"/media/luiz/Volume/Dokumente/Musik/Projekte/stwb_major-scale_v33.wav"
+outputPath = r"/media/luiz/Volume/Dokumente/Musik/Projekte/temp"
+
+audio = AudioSegment.from_wav(audioPath)
+durations_ms = np.array([])
+pause_ms = pause * 60/tempo * 1000
+pause_ms = pause_ms - 1 # fine tuning, not sure why it's needed
+for i, pitch in enumerate(degrees):
+    duration_ms = duration_decay(i) * 60/tempo * 1000
+    durations_ms = np.append(durations_ms, duration_ms)
+    
+for i, pitch in enumerate(degrees):
+    t0 = i*pause_ms + np.sum(durations_ms[:i])
+    t1 = i*pause_ms + np.sum(durations_ms[:i+1])
+    
+    if i > 0:
+        t0 = t0 - 500 # start a bit earlier, to avoid cutting sound
+    
+    fileName = "normal_pitch" + str(pitch) + "_v" + str(1) + ".wav"
+    filePath = path.join(outputPath, fileName)
+    
+    audioSegment = audio[t0:t1]
+    audioSegment.export(filePath, format="wav")
+    
